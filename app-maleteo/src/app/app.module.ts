ebcc1096a9a9cc46ffaf6441f3162e7c67a8a0e1
German <germanux@gmail.com>
2019-10-21 10:49:22 +0200

    App maleteo con form registro.

---
+++ app-maleteo/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { RegistroComponent } from './registro/registro.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RegistroComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
